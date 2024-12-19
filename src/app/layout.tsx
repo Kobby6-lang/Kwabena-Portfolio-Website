497d0ed253c8afa1436026f8552bdc377e680f2e
Kobby6-lang <koseitutu43@gmail.com>
2024-12-19 12:24:43 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head><link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Charm&family=Comic+Neue&family=IBM+Plex+Sans&family=Itim&family=Open+Sans:ital,wght@0,300..800;1,
+    300..800&family=Poppins&display=swap" rel="stylesheet"/></Head>
       <body > {children}</body>
     </html>
   );
